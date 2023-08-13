5a34fea96aba6daeabc3fa604ffb169ac2af171d
stefan <stefan@stefan.com>
2023-08-13 18:49:08 +0200

    added rewards section

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,21 @@ import Hero from "@/app/components/Hero";
 import esbkr from "../assets/esbkr.webp";
 import Image from "next/image";
 import ImageGallery from "@/app/components/ImageGallery";
+import RewardsContainer from "./components/RewardsContainer";
 export default function Home() {
   return (
     <>
-      <div className="  w-full pt-20 h-screen">
+      <div className=" p-20 w-full pt-20 h-screen">
         {" "}
         <Hero />
         <About />
-        <div className="w-full flex flex-col">
+        <h2 className=" text-center py-20 font-thin text-3xl md:text-4xl">
+          Reward Your Body
+        </h2>
+        <div className="w-full flex flex-col md:flex-row justify-around">
           {" "}
-          <h2 className=" text-center py-10 font-thin text-3xl">
-            Reward Your Body
-          </h2>
-          <Image className="p-8" src={esbkr} alt="eat sleep be kind repeat" />
+          <RewardsContainer />
+          <Image className="p-8 " src={esbkr} alt="eat sleep be kind repeat" />
         </div>
         <ImageGallery />
       </div>
--- data/bodyRewards.ts
+++ data/bodyRewards.ts
@@ -19,6 +19,21 @@ const rewards: Rewards[] = [
     content:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
   },
+  {
+    id: 3,
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+  {
+    id: 3,
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+  {
+    id: 3,
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
 ];
 
 export default rewards;
