c392bc3609adfb3b5efc536d300c892ffa7b0d6f
stefan <stefan@stefan.com>
2023-08-13 18:51:16 +0200

    fixed page padding

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,14 @@ import RewardsContainer from "./components/RewardsContainer";
 export default function Home() {
   return (
     <>
-      <div className=" p-20 w-full pt-20 h-screen">
+      <div className="  w-full pt-20 h-screen">
         {" "}
         <Hero />
         <About />
         <h2 className=" text-center py-20 font-thin text-3xl md:text-4xl">
           Reward Your Body
         </h2>
-        <div className="w-full flex flex-col md:flex-row justify-around">
+        <div className="p-20 w-full flex flex-col md:flex-row justify-around">
           {" "}
           <RewardsContainer />
           <Image className="p-8 " src={esbkr} alt="eat sleep be kind repeat" />
