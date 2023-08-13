eb5c31223c9b2e35889b9f4e4f4d2ee56f79466d
stefan <stefan@stefan.com>
2023-08-13 17:45:10 +0200

    changed navbar button text / implemented galery

--- app/components/ImageGallery.tsx
+++ app/components/ImageGallery.tsx
@@ -6,8 +6,8 @@ type Props = {};
 
 function ImageGallery({}: Props) {
   return (
-    <div className="mb-56  p-5 flex overflow-scroll scroll-smooth">
-      <Image className="p-2" src={larisa0} alt={""} />
+    <div className="mb-56  p-5 flex overflow-scroll scroll-smooth snap-x">
+      <Image className="p-2 w-40" src={larisa0} alt={""} />
       <Image className="p-2" src={larisa0} alt={""} />
       <Image className="p-2" src={larisa0} alt={""} />
       <Image className="p-2" src={larisa0} alt={""} />
