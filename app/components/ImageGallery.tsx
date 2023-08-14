19970608c28676f41b095c97f697f0c91a256120
stefan <stefan@stefan.com>
2023-08-14 20:58:34 +0200

    partialy complete complete image modal

--- app/components/ImageGallery.tsx
+++ app/components/ImageGallery.tsx
@@ -1,47 +1,53 @@
-import React from "react";
-import Image from "next/image";
+"use client";
+import React, { useState } from "react";
+import Image, { StaticImageData } from "next/image";
 import larisa0 from "@/assets/larisa0.webp";
+import PictureModal from "./PictureModal"; // Import the PictureModal component
 
-type Props = {};
+type Props = {
+  picture: StaticImageData;
+};
 
 function ImageGallery({}: Props) {
+  // State to manage the selected picture and modal visibility
+  const [selectedPicture, setSelectedPicture] = useState(null);
+
+  // Function to toggle the modal visibility
+  const toggleModal = () => {
+    setSelectedPicture(null);
+  };
+
+  // Function to open the modal with the selected picture
+  const openModal = (picture) => {
+    setSelectedPicture(picture);
+  };
+
   return (
-    <section className=" mt-20 p-10 shadow-2xl grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 grid-rows-3 gap-4 row-start-2  scroll-smooth ">
-      <Image
-        className="hover:-translate-y-2 transition-transform duration-300"
-        src={larisa0}
-        alt={""}
-      />
-      <Image
-        className="hover:-translate-y-2 transition-transform duration-300"
-        src={larisa0}
-        alt={""}
-      />
-      <Image
-        className="hover:-translate-y-2 transition-transform duration-300"
-        src={larisa0}
-        alt={""}
-      />
-      <Image
-        className="hover:-translate-y-2 transition-transform duration-300"
-        src={larisa0}
-        alt={""}
-      />
-      <Image
-        className="hover:-translate-y-2 transition-transform duration-300"
-        src={larisa0}
-        alt={""}
-      />
-      <Image
-        className="hover:-translate-y-2 transition-transform duration-300"
-        src={larisa0}
-        alt={""}
-      />
-      <Image
-        className="hover:-translate-y-2 transition-transform duration-300"
-        src={larisa0}
-        alt={""}
-      />
+    <section className="mt-20 p-10 shadow-2xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-rows-3 gap-4 row-start-2 scroll-smooth">
+      {/* Map through your images and open the selected picture in modal */}
+      {[larisa0, larisa0, larisa0, larisa0, larisa0, larisa0, larisa0].map(
+        (picture, index) => (
+          <div
+            key={index}
+            className="hover:-translate-y-2 transition-transform duration-300 cursor-pointer"
+            onClick={() => openModal(picture)}>
+            <Image
+              src={picture}
+              alt={""}
+              className="w-full h-full object-contain"
+            />
+          </div>
+        )
+      )}
+
+      {/* Render the PictureModal component */}
+      {selectedPicture && (
+        <PictureModal
+          picture={selectedPicture}
+          alt={""}
+          toggleModal={toggleModal}
+        />
+      )}
     </section>
   );
 }
--- app/components/PictureModal.tsx
+++ app/components/PictureModal.tsx
@@ -1,17 +1,32 @@
 "use client";
+import { StaticImageData } from "next/image";
 import React, { useState } from "react";
+import Image from "next/image";
 
-type Props = {};
+type Props = {
+  picture: StaticImageData;
+  alt: string;
+  toggleModal: () => void; // Define the function type correctly
+};
 
-function PictureModal({}: Props) {
-  const [showModal, setShowModal] = useState(false);
-  return (
-    showModal && (
-      <div className="fixed inset-0 h-screen w-full bg-black opacity-50 z-10">
-        <div className="absolute inset-1/4 bg-white"></div>
-      </div>
-    )
-  );
+function PictureModal({ picture, alt, toggleModal }: Props) {
+  // Use local state to manage the modal visibility
+  const [showModal, setShowModal] = useState(true);
+
+  // Function to toggle the modal visibility
+  const handleToggleModal = () => {
+    setShowModal(!showModal);
+    toggleModal(); // Call the passed toggleModal function from props
+  };
+
+  return showModal ? (
+    <div
+      onClick={handleToggleModal}
+      className="fixed inset-0 h-screen w-full bg-black  z-10">
+      {/* Center the Image vertically and horizontally */}
+      <Image src={picture} alt={alt} className="w-full h-full object-contain" />
+    </div>
+  ) : null;
 }
 
 export default PictureModal;
