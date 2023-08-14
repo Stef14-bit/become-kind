3ae7654181056822fd41daf948c2cdef9a45c7e8
stefan <stefan@stefan.com>
2023-08-14 17:09:18 +0200

    galery page complete

--- app/components/ImageGallery.tsx
+++ app/components/ImageGallery.tsx
@@ -6,13 +6,42 @@ type Props = {};
 
 function ImageGallery({}: Props) {
   return (
-    <section className=" h-screen p-5 grid grid-cols-2 grid-rows-3 gap-4 row-start-2 overflow-scroll scroll-smooth">
-      <Image className="p-2 w-40" src={larisa0} alt={""} />
-      <Image className="p-2" src={larisa0} alt={""} />
-      <Image className="p-2" src={larisa0} alt={""} />
-      <Image className="p-2" src={larisa0} alt={""} />
-      <Image className="p-2" src={larisa0} alt={""} />
-      <Image className="p-2" src={larisa0} alt={""} />
+    <section className=" mt-20 p-10 shadow-2xl grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 grid-rows-3 gap-4 row-start-2  scroll-smooth ">
+      <Image
+        className="hover:-translate-y-2 transition-transform duration-300"
+        src={larisa0}
+        alt={""}
+      />
+      <Image
+        className="hover:-translate-y-2 transition-transform duration-300"
+        src={larisa0}
+        alt={""}
+      />
+      <Image
+        className="hover:-translate-y-2 transition-transform duration-300"
+        src={larisa0}
+        alt={""}
+      />
+      <Image
+        className="hover:-translate-y-2 transition-transform duration-300"
+        src={larisa0}
+        alt={""}
+      />
+      <Image
+        className="hover:-translate-y-2 transition-transform duration-300"
+        src={larisa0}
+        alt={""}
+      />
+      <Image
+        className="hover:-translate-y-2 transition-transform duration-300"
+        src={larisa0}
+        alt={""}
+      />
+      <Image
+        className="hover:-translate-y-2 transition-transform duration-300"
+        src={larisa0}
+        alt={""}
+      />
     </section>
   );
 }
