7afc9e38034d181e076ed2ed4b647c2698e7686e
Stefan.Iv PC <stefan.ivanus@outlook.de>
2023-08-20 18:22:53 +0200

    footer text issue fix

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,7 +6,8 @@ function Footer({}: Props) {
   return (
     <footer className="w-full border-gray-400 border-t-2 border-b-4 h-32 flex flex-col items-center justify-center font-light bg-red-50 relative">
       <p>becomekind@proton.me</p>
-      <p>©2023 by BE KIND. Proudly presented by becomekind.com</p>
+      <p>©2023 by BE KIND</p>
+      <p>Proudly presented by becomekind.com</p>
     </footer>
   );
 }
