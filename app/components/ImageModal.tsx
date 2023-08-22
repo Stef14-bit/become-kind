cfd28f9bd3b57576862141207bc7d9d6dd2e3522
stefan <stefan@stefan.com>
2023-08-22 19:36:47 +0200

    image view modal rebuild. added key to gallery obj

--- app/components/ImageModal.tsx
+++ app/components/ImageModal.tsx
@@ -26,37 +26,13 @@ function ImageModal({ currentIndex, onCloseModal }: Props) {
   };
 
   return (
-    <div className="fixed max-h-screen w-full inset-0 bg-black/50 flex flex-col items-center justify-center p-5">
-      <Image
-        src={galleryImages[currentIndex].url}
-        alt=""
-        width={1000}
-        height={1000}
-        layout="responsive"
-      />
-      <div className="hover:bg-black/75 absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-        <BsChevronCompactLeft size={30} onClick={prevSlide} />
-      </div>
-      <div className="hover:bg-black/75 absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-        <BsChevronCompactRight size={30} onClick={nextSlide} />
-      </div>
-      <div className="flex top-4 justify-center p-2">
-        {galleryImages.map((slide, slideIndex) => (
-          <div
-            key={slideIndex}
-            className="text-2xl cursor-pointer flex content-center m-5 justify-center"
-            onClick={() => goToSlide(slideIndex)}>
-            <div className="w-100 h-100 max-w-[100px] max-h-[100px]">
-              <Image src={slide.url} alt="" width={100} height={100} />
-            </div>
-          </div>
+    <div className="fixed h-screen w-full top-0 z-20 left-0 bg-black/50 flex flex-col items-center justify-center  p-5">
+      <div className="h-4/5 w-full bg-blue-500 m-5">
+        {galleryImages.map((image) => (
+          <Image key={index} src={image.url} alt="" />
         ))}
       </div>
-      <button
-        className="absolute top-5 right-5 text-2xl text-white cursor-pointer"
-        onClick={onCloseModal}>
-        Close
-      </button>
+      <div className="h-1/5 w-full bg-red-500 mb-5"></div>
     </div>
   );
 }
