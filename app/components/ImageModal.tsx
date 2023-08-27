da51e470c9945694fcaa054ab9f77d2f2fb4f048
stefan <stefan@stefan.com>
2023-08-27 12:05:21 +0200

    working image view modal

--- app/components/ImageModal.tsx
+++ app/components/ImageModal.tsx
@@ -11,27 +11,27 @@ type Props = {
 
 function ImageModal({ currentIndex, onCloseModal, newImage }: Props) {
   const activeHeight = (index: number) => {
-    return currentIndex === index ? 50 : 25;
+    return currentIndex === index ? 50 : 30;
   };
   const activeWidth = (index: number) => {
-    return currentIndex === index ? 100 : 40;
+    return currentIndex === index ? 100 : 60;
   };
   const nextPicture = () => {
     if (currentIndex < galleryImages.length - 1) {
-      newImage(currentIndex + 1); // Update the currentIndex using newImage
+      newImage(currentIndex + 1);
     }
   };
 
   const prevPicture = () => {
     if (currentIndex > 0) {
-      newImage(currentIndex - 1); // Update the currentIndex using newImage
+      newImage(currentIndex - 1);
     }
   };
 
   return (
-    <div className="fixed h-screen w-full top-0 z-20 left-0 bg-black/50 flex flex-col  p-5 justify-center">
+    <div className="fixed h-screen w-full top-0 z-20 left-0 bg-black flex flex-col p-5 justify-center">
       <button
-        className="absolute top-0 right-5 p-8 text-white"
+        className="absolute top-0 right-5 p-2 text-white bg-black"
         onClick={onCloseModal}>
         close
       </button>
@@ -45,20 +45,20 @@ function ImageModal({ currentIndex, onCloseModal, newImage }: Props) {
         {" "}
         <button
           onClick={nextPicture}
-          className="text-white bg-black/70 p-3 rounded-full absolute top-[50%] right-8 font-extrabold text-3xl">
+          className="text-white bg-black/70 p-3 rounded-f absolute top-[50%] right-8 font-extrabold text-3xl">
           <BsChevronCompactRight></BsChevronCompactRight>
         </button>
         <button
           onClick={prevPicture}
-          className="text-white bg-black/70 p-3 rounded-full absolute top-[50%] left-8 font-extrabold text-3xl">
+          className="text-white bg-black/70 p-3 rounded-f absolute top-[50%] left-8 font-extrabold text-3xl">
           <BsChevronCompactLeft></BsChevronCompactLeft>
         </button>
       </div>
-      <div className="h-1/4 w-full bg-black mb-5 overflow-x-scroll scroll-smooth flex justify-center content-center ">
+      <div className="h-1/4 w-full bg-black mb-5 overflow-x-auto scroll-smooth flex justify-center content-center ">
         {galleryImages.map((image, index) => (
           <Image
             onClick={() => newImage(index)}
-            className="m-2"
+            className={`m-2`}
             width={activeWidth(index)}
             height={activeHeight(index)}
             key={index}
