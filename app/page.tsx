999cd9df5e55f87cdcfff6638d559350e8a6574b
stefan <stefan@stefan.com>
2023-09-01 16:24:27 +0200

    build deployment fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import About from "@/app/components/AboutUs";
 import Hero from "@/app/components/Hero";
 import RewardsContainer from "./components/RewardsContainer";
-import ImageSlider from "./components/ImageSlider";
 export default function Home() {
   return (
     <>
