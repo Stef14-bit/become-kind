75c3488ebc4696dc9a59343a7e95339bb25b854e
stefan <stefan@stefan.com>
2023-09-01 15:57:02 +0200

    ^basic contact page implemented

--- app/contact_us/page.tsx
+++ app/contact_us/page.tsx
@@ -1,65 +1,36 @@
 import React from "react";
+import Button from "../components/Button";
 
 type Props = {};
 
 function page({}: Props) {
   return (
-    <section className="overflow-auto">
-      <div className="py-8 lg:py-16 px-4 mx-auto max-w-full-md">
-        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
-          Contact Us
-        </h2>
-        <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
-          Let us know whats on your mind
-        </p>
-        <form className="space-y-8">
-          <div>
-            <label
-              htmlFor="email"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-              Your email
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 "
-              placeholder="name@flowbite.com"
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="subject"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-              Subject
-            </label>
-            <input
-              type="text"
-              id="subject"
-              className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500"
-              placeholder="Let us know how we can help you"
-              required
-            />
-          </div>
-          <div className="sm:col-span-2">
-            <label
-              htmlFor="message"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">
-              Your message
-            </label>
-            <textarea
-              id="message"
-              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500s "
-              placeholder="Leave a comment..."></textarea>
-          </div>
-          <button
-            type="submit"
-            className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-            Send message
-          </button>
-        </form>
+    <form className="p-10 text-xl font-light">
+      <div className="flex flex-col justify-center items-center py-5 ">
+        <label htmlFor="email">Email</label>
+        <input
+          className="border-solid border-1 bg-red-50 w-3/4 rounded-md h-10 my-1"
+          type="email"
+          id="email"
+        />
       </div>
-    </section>
+      <div className="flex flex-col justify-center items-center py-5 ">
+        <label htmlFor="name">Your name</label>
+        <input
+          className="border-solid border-1 bg-red-50 w-3/4 rounded-md h-10 my-1"
+          type="text"
+          id="name"
+        />
+      </div>
+      <div className="flex flex-col justify-center items-center py-5 ">
+        <label htmlFor="message">Your message</label>
+        <textarea
+          className="border-solid border-1 bg-red-50 resize rounded-md w-full my-1 max-w-full"
+          id="message"
+        />
+      </div>
+      <Button>Submit</Button>
+    </form>
   );
 }
 
