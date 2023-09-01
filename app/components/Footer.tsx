8b24ce8aebe25e34d72ff208e109933b81473548
stefan <stefan@stefan.com>
2023-09-01 16:11:30 +0200

    minor footer visual fix / dead code removal

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,15 +5,14 @@ type Props = {};
 
 function Footer({}: Props) {
   return (
-    <footer className="w-full border-gray-400 border-t-2 border-b-4 h-32 flex justify-around items-center font-light bg-red-50 ">
+    <footer className="w-full border-gray-400 border-t-2 border-b-4 h-32 flex justify-around items-center font-light bg-red-50">
       <div className=" flex flex-col justify-center content-center p-5 text-center">
-        {" "}
-        <Link className="p-2" href={"/about_us/"}>
+        <Link className="p-1" href={"/about_us/"}>
           About
         </Link>
-        <Link className="p-2" href={"/contact_us"}>
+        <Link className="p-1" href={"/contact_us"}>
           Contact
-        </Link>{" "}
+        </Link>
         <p>©2023 BE KIND</p>
       </div>
     </footer>
